4ad2564d8c175052bfb9cc93c77c9c1f3233c454
Karl Voit <git@Karl-Voit.at>
2014-03-01 23:08:26 +0100

    example_invocation.sh updated

--- example_invocation.sh
+++ example_invocation.sh
@@ -3,6 +3,7 @@
 PYTHONPATH="~/src/lazyblorg:" ./lazyblorg.py \
     --aboutblog "lazyblorg test blog" \
     --targetdir testdata/2del \
-    --metadata ./2del-metadata.pk \
+    --previous-metadata ./NONEXISTING.pk \
+    --new-metadata ./2del-metadata.pk \
     --logfile ./2del-logfile.org \
     --orgfiles testdata/manual_prototype/org/test.org ./templates/blog-format.org $@ 
