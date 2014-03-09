5381aceff88a99537995ac6c442b97d00368e377
Karl Voit <git@Karl-Voit.at>
2014-03-09 19:52:57 +0100

    example-script: fixed issue with internal link in footer

--- example_invocation.sh
+++ example_invocation.sh
@@ -8,4 +8,4 @@ PYTHONPATH="~/src/lazyblorg:" ./lazyblorg.py \
     --previous-metadata ./NONEXISTING.pk \
     --new-metadata ./2del-metadata.pk \
     --logfile ./2del-logfile.org \
-    --orgfiles testdata/manual_prototype/org/test.org ./templates/blog-format.org $@ 
+    --orgfiles testdata/manual_prototype/org/test.org ./testdata/about-placeholder.org ./templates/blog-format.org $@ 
