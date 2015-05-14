0d90529669cac2e3216359a328c45d48a26f77df
Karl Voit <git@Karl-Voit.at>
2015-05-14 20:09:57 +0200

    orgparser_test.py: fixed formatting issues

--- lib/tests/orgparser_test.py
+++ lib/tests/orgparser_test.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8; mode: python; -*-
-# Time-stamp: <2014-04-20 21:56:19 vk>
+# Time-stamp: <2015-05-14 20:09:40 vk>
 
-import config  ## lazyblorg-global settings
+import config  # lazyblorg-global settings
 import unittest
 from lib.utils import *
 from lib.orgparser import *
-import pickle ## for serializing and storing objects into files
+import pickle  # for serializing and storing objects into files
 from os import remove
 from os.path import isfile
 
-## debugging:   for setting a breakpoint:  pdb.set_trace()## FIXXME
-import pdb
 
 class TestOrgParser(unittest.TestCase):
 
@@ -21,30 +19,27 @@ class TestOrgParser(unittest.TestCase):
 
     logging = None
 
-
     def setUp(self):
         verbose = False
         quiet = False
         self.logging = Utils.initialize_logging("lazyblorg", verbose, quiet)
 
-
     def tearDown(self):
         pass
 
-
     def test_simple_org_to_blogdata(self):
 
-        testfile_org = "simple.org"  ## manually written Org-mode file; has to be placed in "lib/tests/"
+        testfile_org = "simple.org"  # manually written Org-mode file; has to be placed in "lib/tests/"
         testfile_temp_output = "simple_org_-_lastrun.pk"
         testfile_temp_reference = "simple_org_-_reference.pk"
 
-        self.assertTrue(isfile(testfile_org))  ## check, if test input file is found
+        self.assertTrue(isfile(testfile_org))  # check, if test input file is found
 
         ## make sure that no old output file is found:
         if isfile(testfile_temp_output):
             remove(testfile_temp_output)
-            
-        blog_data = []  ## initialize the empty list
+
+        blog_data = []  # initialize the empty list
         parser = OrgParser(testfile_org)
 
         ## parse the example Org-mode file:
@@ -65,8 +60,8 @@ class TestOrgParser(unittest.TestCase):
             reference_blog_data = pickle.load(fileinput)
 
         ## a more fine-grained diff (only) on the first element in blog_data:
-        for x in range(len(blog_data[0]['content'])): 
-            if blog_data[0]['content'][x] != reference_blog_data[0]['content'][x]: 
+        for x in range(len(blog_data[0]['content'])):
+            if blog_data[0]['content'][x] != reference_blog_data[0]['content'][x]:
                 print "   =============== difference ==================="
                 print reference_blog_data[0]['content'][x]
                 print "   -------------------------------"
