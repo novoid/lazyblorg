f6ad9c35d914cb8f908c083ef75d418c7f2d8b13
Karl Voit <git@Karl-Voit.at>
2016-11-12 17:29:17 +0100

    testall.sh: fixed small issue

--- testall.sh
+++ testall.sh
@@ -4,7 +4,7 @@
 ## if no error is found, the final success statement is shown
 ## if error occurs, this script stops at the error
 
-./start_unit_tests.sh && start_end-to-end-test.sh && \
+./start_unit_tests.sh && ./start_end-to-end-test.sh && \
 echo "\n\n           All tests ended successfully! :-)\n\n"
 
 #end
